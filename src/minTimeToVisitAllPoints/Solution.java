8a2f9a93dfbcfad89445ecaf3bb417725f5a6a96
liccil76 <1241128661@qq.com>
2020-08-13 19:20:46 +0800

    1266.访问所有点的最小时间

--- src/minTimeToVisitAllPoints/Solution.java
+++ src/minTimeToVisitAllPoints/Solution.java
@@ -33,14 +33,11 @@ public class Solution {
      * points[i].length == 2
      * -1000 <= points[i][0], points[i][1] <= 1000
      */
-    private int sum = 0;
     public int minTimeToVisitAllPoints(int[][] points) {
+        int sum = 0;
         for (int i = 0; i < points.length-1; i++) {
-            sum += temp(points[i],points[i+1]);
+            sum += Math.max(Math.abs(points[i][0]-points[i+1][0]),Math.abs(points[i][1]-points[i+1][1]));
         }
         return sum;
-    }
-    public int temp(int[] one,int[] two){
-        return Math.max(Math.abs(one[0]-two[0]),Math.abs(one[1]-two[1]));
     }
 }
