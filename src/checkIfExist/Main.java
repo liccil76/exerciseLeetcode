9cb001e70da7bc7252106ec6acd5ff235f245a51
liccil76 <1241128661@qq.com>
2020-08-14 17:17:49 +0800

    1346. 检查整数及其两倍数是否存在

---
+++ src/checkIfExist/Main.java
@@ -0,0 +1,9 @@
+package checkIfExist;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = {3,1,7,11};
+        boolean b = new Solution().checkIfExist2(arr);
+        System.out.println(b);
+    }
+}
