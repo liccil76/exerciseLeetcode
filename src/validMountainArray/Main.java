4faba506b2fc3078a3f6c988f1f192e030f9d2bc
liccil76 <1241128661@qq.com>
2020-08-19 14:59:53 +0800

    941. 有效的山脉数组

---
+++ src/validMountainArray/Main.java
@@ -0,0 +1,9 @@
+package validMountainArray;
+
+public class Main {
+    public static void main(String[] args){
+        int[] A = {0,1,2,3,4,5,6,7,8,9};
+        boolean b = new Solution().validMountainArray(A);
+        System.out.println(b);
+    }
+}
