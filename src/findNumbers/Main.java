52e834d271fa3a6a1976ad06a68e6bd5d9da0101
liccil76 <1241128661@qq.com>
2020-08-20 13:51:07 +0800

    1295. 统计位数为偶数的数字

---
+++ src/findNumbers/Main.java
@@ -0,0 +1,9 @@
+package findNumbers;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {555,901,482,1771};
+        int numbers = new Solution().findNumbers(nums);
+        System.out.println(numbers);
+    }
+}
