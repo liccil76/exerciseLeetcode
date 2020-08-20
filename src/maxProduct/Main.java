acb0b25243da05d4d91b7bb0cf8944f2d3d7e0b4
liccil76 <1241128661@qq.com>
2020-08-20 18:00:46 +0800

    1464. 数组中两元素的最大乘积

---
+++ src/maxProduct/Main.java
@@ -0,0 +1,9 @@
+package maxProduct;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {3,4,5,2};
+        int i = new Solution().maxProduct(nums);
+        System.out.println(i);
+    }
+}
