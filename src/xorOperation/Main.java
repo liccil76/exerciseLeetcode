efd0c1b18cdfe26b5f6ba3936ffcb79780378271
liccil76 <1241128661@qq.com>
2020-08-21 15:41:48 +0800

    1486. 数组异或操作

---
+++ src/xorOperation/Main.java
@@ -0,0 +1,9 @@
+package xorOperation;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 5;
+        int start = 0;
+        System.out.println(new Solution().xorOperation(n,start));
+    }
+}
