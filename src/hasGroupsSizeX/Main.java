9f54bb7ec1dcc6e6413d50276a7c37e8b853fe4c
liccil76 <1241128661@qq.com>
2020-08-25 17:24:49 +0800

    914. 卡牌分组

---
+++ src/hasGroupsSizeX/Main.java
@@ -0,0 +1,9 @@
+package hasGroupsSizeX;
+
+public class Main {
+    public static void main(String[] args){
+        int[] deck = {1,1,2,2,2,2};
+        boolean b = new Solution().hasGroupsSizeX(deck);
+        System.out.println(b);
+    }
+}
