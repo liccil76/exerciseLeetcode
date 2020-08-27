22071e27fb4624c6be1a337ec6ee06209d7dd38a
liccil76 <1241128661@qq.com>
2020-08-27 10:12:25 +0800

    509. 斐波那契数

---
+++ src/fib/Main.java
@@ -0,0 +1,9 @@
+package fib;
+
+public class Main {
+    public static void main(String[] args){
+        int N = 4;
+        int fib = new Solution().fib(N);
+        System.out.println(fib);
+    }
+}
