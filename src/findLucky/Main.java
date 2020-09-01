2bb6cea3e4942507a2835b70a99abd0bff146026
liccil76 <1241128661@qq.com>
2020-09-01 10:03:22 +0800

    1394. 找出数组中的幸运数

---
+++ src/findLucky/Main.java
@@ -0,0 +1,8 @@
+package findLucky;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = {7,7,7,7,7,7,7};
+        System.out.println(new Solution().findLucky(arr));
+    }
+}
