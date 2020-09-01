c5c4622e2098b5da92697ab71b3f08ed5f6394b1
liccil76 <1241128661@qq.com>
2020-09-01 15:33:29 +0800

    1502. 判断能否形成等差数列

---
+++ src/canMakeArithmeticProgression/Main.java
@@ -0,0 +1,9 @@
+package canMakeArithmeticProgression;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = {3,5,1};
+        boolean b = new Solution().canMakeArithmeticProgression(arr);
+        System.out.println(b);
+    }
+}
