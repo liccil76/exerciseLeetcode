159c68d1ff8cd332bf3c37a493e6b642e5a8e77c
liccil76 <1241128661@qq.com>
2020-09-01 15:12:06 +0800

    1413. 逐步求和得到正数的最小值

---
+++ src/minStartValue/Main.java
@@ -0,0 +1,9 @@
+package minStartValue;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {-3,2,-3,4,2};
+        int i = new Solution().minStartValue(nums);
+        System.out.println(i);
+    }
+}
