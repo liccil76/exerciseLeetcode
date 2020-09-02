a2b9b3d712b0c7e23d195040c025179c895b4a55
liccil76 <1241128661@qq.com>
2020-09-02 19:38:29 +0800

    剑指 Offer 53 - I. 在排序数组中查找数字 I

---
+++ src/search/Main.java
@@ -0,0 +1,9 @@
+package search;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {5,7,7,8,8,10};
+        int target = 10;
+        System.out.println(new Solution().search(nums,target));
+    }
+}
---
+++ src/search/Solution.java
@@ -0,0 +1,44 @@
+package search;
+
+public class Solution {
+    /**
+     * 剑指 Offer 53 - I. 在排序数组中查找数字 I
+     * 统计一个数字在排序数组中出现的次数。
+     *
+     * 示例 1:
+     * 输入: nums = [5,7,7,8,8,10], target = 8
+     * 输出: 2
+     *
+     * 示例 2:
+     * 输入: nums = [5,7,7,8,8,10], target = 6
+     * 输出: 0
+     *
+     * 限制：
+     * 0 <= 数组长度 <= 50000
+     */
+    //二分+递归
+    public int search(int[] nums, int target) {
+        // 搜索右边界 right
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] <= target) i = m + 1;
+            else j = m - 1;
+        }
+        int right = i;
+
+        // 若数组中无 target ，则提前返回
+        if(j >= 0 && nums[j] != target) return 0;
+
+        // 搜索左边界 right
+        i = 0; j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] < target) i = m + 1;
+            else j = m - 1;
+        }
+        int left = j;
+
+        return right - left - 1;
+    }
+}
