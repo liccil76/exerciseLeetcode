1092a40b273ef6cef83c46b02c658c245fdf5152
liccil76 <1241128661@qq.com>
2020-09-03 09:48:44 +0800

    122. 买卖股票的最佳时机 II

--- src/maxProfit/Main.java
+++ src/maxProfit/Main.java
@@ -2,7 +2,10 @@ package maxProfit;
 
 public class Main {
     public static void main(String[] args){
-        int[] prices = {1,4};
-        System.out.println(new Solution().maxProfit(prices));
+//        int[] prices = {1,4};
+//        System.out.println(new Solution().maxProfit(prices));
+
+        int[] prices = {7,6,4,3,1};
+        System.out.println(new Solution2().maxProfit(prices));
     }
 }
