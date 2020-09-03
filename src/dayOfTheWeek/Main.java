7cd1779b3889d92e58071bd39ba686ceeee3bace
liccil76 <1241128661@qq.com>
2020-09-03 15:31:52 +0800

    1185. 一周中的第几天

---
+++ src/dayOfTheWeek/Main.java
@@ -0,0 +1,10 @@
+package dayOfTheWeek;
+
+public class Main {
+    public static void main(String[] args){
+        int day = 29;
+        int month = 2;
+        int year = 2016;
+        System.out.println(new Solution().dayOfTheWeek(day,month,year));
+    }
+}
