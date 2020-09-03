11add741c0328be52b6cfc931b134fde7845ae83
liccil76 <1241128661@qq.com>
2020-09-03 14:40:39 +0800

    1566. 重复至少 K 次且长度为 M 的模式

---
+++ src/containsPattern/Main.java
@@ -0,0 +1,10 @@
+package containsPattern;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = {2,2,2,2};
+        int m = 2;
+        int k = 3;
+        System.out.println(new Solution().containsPattern(arr,m,k));
+    }
+}
