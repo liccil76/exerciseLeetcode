700e6701a4f5dc3be80988408c4e5801501c0737
liccil76 <1241128661@qq.com>
2020-09-07 18:02:21 +0800

    1572. 矩阵对角线元素的和

---
+++ src/diagonalSum/Main.java
@@ -0,0 +1,8 @@
+package diagonalSum;
+
+public class Main {
+    public static void main(String[] args){
+        int[][] mat = {{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}};
+        System.out.println(new Solution().diagonalSum(mat));
+    }
+}
