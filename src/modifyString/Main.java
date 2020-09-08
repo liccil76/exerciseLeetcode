907e063a8d6b4b1022bb5cfa661f9b2559c6548a
liccil76 <1241128661@qq.com>
2020-09-08 16:49:45 +0800

    1576. 替换所有的问号

---
+++ src/modifyString/Main.java
@@ -0,0 +1,9 @@
+package modifyString;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "a?";
+        String s1 = new Solution().modifyString(s);
+        System.out.println(s1);
+    }
+}
