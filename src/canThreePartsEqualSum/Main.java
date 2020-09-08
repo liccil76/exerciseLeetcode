e2fcfda45bd696cbc650992172423d85ae5b6ecc
liccil76 <1241128661@qq.com>
2020-09-08 09:05:50 +0800

    1013. 将数组分成和相等的三个部分

---
+++ src/canThreePartsEqualSum/Main.java
@@ -0,0 +1,8 @@
+package canThreePartsEqualSum;
+
+public class Main {
+    public static void main(String[] args){
+        int[] A = {1,-1,1,-1};
+        System.out.println(new Solution().canThreePartsEqualSum(A));
+    }
+}
