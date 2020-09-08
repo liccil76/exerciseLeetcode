0938575ce093f5e66c51c7bf81561672822f70a5
liccil76 <1241128661@qq.com>
2020-09-08 10:57:53 +0800

    665. 非递减数列

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # exerciseLeetcode
 leetcode刷题记录
-已经做了动态规划、树的easy部分
-现在在做数组的easy部分
+
+已经做完：
+
+    1、动态规划（easy）
+
+    2、树（easy）
+    
+    3、数组（easy）
+
+正在做：
+
+    1、字符串（easy）
---
+++ src/checkPossibility/Main.java
@@ -0,0 +1,9 @@
+package checkPossibility;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {3,4,2,3};
+        boolean b = new Solution().checkPossibility(nums);
+        System.out.println(b);
+    }
+}
