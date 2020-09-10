a900b1e0e1a384d021e344128e7962ac323d6762
liccil76 <1241128661@qq.com>
2020-09-10 09:35:53 +0800

    459. 重复的子字符串

---
+++ src/repeatedSubstringPattern/Main.java
@@ -0,0 +1,8 @@
+package repeatedSubstringPattern;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "bb";
+        System.out.println(new Solution().repeatedSubstringPattern(s));
+    }
+}
