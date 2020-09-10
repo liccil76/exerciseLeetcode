a02506f8c97a63b0343fb0c6482ef069340f81d7
liccil76 <1241128661@qq.com>
2020-09-10 17:25:22 +0800

    1370. 上升下降字符串

---
+++ src/sortString/Main.java
@@ -0,0 +1,8 @@
+package sortString;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "aaaabbbbcccc";
+        System.out.println(new Solution().sortString(s));
+    }
+}
