d81fd8fff93c015013757e9a3b2c4ab7c03d09cf
liccil76 <1241128661@qq.com>
2020-09-11 13:36:24 +0800

    1374. 生成每种字符都是奇数个的字符串

---
+++ src/generateTheString/Main.java
@@ -0,0 +1,8 @@
+package generateTheString;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 2;
+        System.out.println(new Solution().generateTheString(n));
+    }
+}
