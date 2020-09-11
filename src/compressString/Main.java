49feb35e0c188ea62e32311c4e00a6832ce53669
liccil76 <1241128661@qq.com>
2020-09-11 09:46:52 +0800

    面试题 01.06. 字符串压缩

---
+++ src/compressString/Main.java
@@ -0,0 +1,8 @@
+package compressString;
+
+public class Main {
+    public static void main(String[] args){
+        String S = "aabcccccaaa";
+        System.out.println(new Solution().compressString(S));
+    }
+}
