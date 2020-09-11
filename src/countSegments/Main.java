e1843f78fe4b62f47084dbed08cf85e6596b3586
liccil76 <1241128661@qq.com>
2020-09-11 15:41:55 +0800

    434. 字符串中的单词数

---
+++ src/countSegments/Main.java
@@ -0,0 +1,8 @@
+package countSegments;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "love live! mu'sic forever";
+        System.out.println(new Solution().countSegments(s));
+    }
+}
