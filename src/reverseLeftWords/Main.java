f5a5951d1d2b54b14130dd480e160781de893456
liccil76 <1241128661@qq.com>
2020-09-15 15:44:18 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ src/reverseLeftWords/Main.java
@@ -0,0 +1,9 @@
+package reverseLeftWords;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "abcdefg";
+        int n = 2;
+        System.out.println(new Solution().reverseLeftWords(s,n));
+    }
+}
