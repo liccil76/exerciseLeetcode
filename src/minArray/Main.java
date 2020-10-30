6887f0e2c8a3c1433242712200c60a65f53f4b9b
liccil76 <1241128661@qq.com>
2020-10-30 18:20:16 +0800

    旋转数组的最小数字

---
+++ src/minArray/Main.java
@@ -0,0 +1,8 @@
+package minArray;
+
+public class Main {
+    public static void main(String[] args){
+        int[] numbers = {3,4,5,1,2};
+        System.out.println(new Solution().minArray(numbers));
+    }
+}
