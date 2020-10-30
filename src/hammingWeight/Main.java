3407e9689fae0a6882c07e622e645fc593aad829
liccil76 <1241128661@qq.com>
2020-10-30 18:38:30 +0800

    二进制中1的个数

---
+++ src/hammingWeight/Main.java
@@ -0,0 +1,8 @@
+package hammingWeight;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 00000000000000000000000000001011;
+        System.out.println(new Solution().hammingWeight(n));
+    }
+}
