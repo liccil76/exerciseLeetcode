3650d3968f4fa01c924572d1fbe327303b2b96cf
lichen <1241128661@qq.com>
2020-11-14 21:43:43 +0800

    剑指 Offer 58 - I. 翻转单词顺序

--- src/reverseWords/Main.java
+++ src/reverseWords/Main.java
@@ -2,7 +2,8 @@ package reverseWords;
 
 public class Main {
     public static void main(String[] args){
-        String s = "Let's take LeetCode contest";
-        System.out.println(new Solution().reverseWords(s));
+        String s = "";
+//        System.out.println(new Solution().reverseWords(s));
+        System.out.println(new Solution2().reverseWords(s));
     }
 }
