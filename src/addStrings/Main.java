503244f6b839d9dde90e58e30a1c5c9145815e6d
1241128661@qq.com <li19930923chen>
2021-03-12 17:51:58 +0800

    415. 字符串相加

---
+++ src/addStrings/Main.java
@@ -0,0 +1,10 @@
+package addStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        String num1 = "12";
+        String num2 = "9";
+        String s = new Solution().addStrings(num1, num2);
+        System.out.println(s);
+    }
+}
