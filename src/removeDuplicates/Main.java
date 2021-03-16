d6ff4bf28a2616c54224eb06150d9520886271fa
1241128661@qq.com <li19930923chen>
2021-03-16 10:26:15 +0800

    1047. 删除字符串中的所有相邻重复项（使用栈）

---
+++ src/removeDuplicates/Main.java
@@ -0,0 +1,9 @@
+package removeDuplicates;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abbaca";
+        String s = new Solution().removeDuplicates(str);
+        System.out.println(s);
+    }
+}
