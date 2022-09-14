26858af835dbd6febefc9237c6432665259cffeb
lichen <1241128661@qq.com>
2022-09-14 16:10:54 +0800

    merge-sorted-array merge方法（正序、双指针、临时数组） merge2方法（倒序、双指针、不使用额外空间）

--- src/mergesortedarray/Solution.java
+++ src/mergesortedarray/Solution.java
@@ -14,36 +14,56 @@ public class Solution {
      * nums2 = [2,5,6],       n = 3
      * 输出: [1,2,2,3,5,6]
      */
+
+    /**
+     * 正序、双指针、临时数组
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int one = 0;
-        int two = 0;
-        int[] temp = new int[m];
-        System.arraycopy(nums1,0,temp,0,m);
-        for (int i = 0; i < nums1.length; i++) {
-            if(one>=m){
-                //nums1已经轮询完
-                if(two<n){
-                    nums1[i]=nums2[two];
-                    two++;
-                }
+        int p1 = 0 ,p2 = 0;
+        int[] temp = new int[m+n];
+        int cur;
+        while(p1<m || p2<n){
+            if(p1==m){
+                cur = nums2[p2++];
+            }else if(p2==n){
+                cur = nums1[p1++];
+            }else if(nums1[p1] > nums2[p2]){
+                cur = nums2[p2++];
             }else{
-                if(two>=n){
-                    //nums2已经轮询完
-                    nums1[i]=temp[one];
-                    one++;
-                }else{
-                    if(temp[one]<nums2[two]){
-                        nums1[i]=temp[one];
-                        one++;
-                    }else if(temp[one]>nums2[two]){
-                        nums1[i]=nums2[two];
-                        two++;
-                    }else if(temp[one]==nums2[two]){
-                        nums1[i]=temp[one];
-                        one++;
-                    }
-                }
+                cur = nums1[p1++];
             }
+            temp[p1+p2-1]=cur;
+        }
+        //临时数组赋值至nums1
+        System.arraycopy(temp, 0, nums1, 0, temp.length);
+    }
+
+    /**
+     * 逆序 双指针 不适用额外空间
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1,p2 = n-1;
+        int tail = m+n-1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0){
+            if(p1==-1){
+                cur = nums2[p2--];
+            }else if(p2==-1){
+                cur = nums1[p1--];
+            }else if(nums1[p1]>nums2[p2]){
+                cur = nums1[p1--];
+            }else{
+                cur = nums2[p2--];
+            }
+            nums1[tail--]=cur;
         }
     }
 }
